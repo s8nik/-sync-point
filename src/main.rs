9dbbc5ad2a171bbe192e4ffefc6ec7d9a347c8bb
Nikita <nick.parser@gmail.com>
2024-07-28 15:10:33 +0200

    main & env

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,15 @@
+use std::net::SocketAddr;
+
+use anyhow::Context;
+
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    Ok(())
+    dotenv::dotenv()?;
+    sync_point::logger::init()?;
+
+    let addr: SocketAddr = std::env::var("SYNC_POINT_ADDR")?
+        .parse()
+        .with_context(|| "invalid sync point addr")?;
+
+    sync_point::serve(addr).await
 }
